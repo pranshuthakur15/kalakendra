9987d02305364b460dbd7c630d382f0dab9f1f79
PRANSHU SINGH THAKUR <pspranshusingh15@gmail.com>
2024-09-20 09:52:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
+
+// src/App.js
 import React from 'react';
-import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
 
-function App() {
-  
+const App = () => {
   return (
-    <BrowserRouter>
-    <div className="App">
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Sections */}
-      <div id="home">
-        <Home />
-      </div>
-
-      <div id="about">
-        <About />
-      </div>
-
-      <div id="services">
-        <Services />
-      </div>
-
-      <div id="portfolio">
-        <Portfolio />
-      </div>
-
-      <div id="contact">
-        <Contact />
-      </div>
-    </div>
-    </BrowserRouter>
+    <Router>
+         <Navbar /> {/* Add Navbar at the top */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home component will render here */}
+        <Route path="/portfolio" element={<Portfolio />} /> {/* Portfolio component will render here */}
+        <Route path="/contact" element={<Contact />} /> {/* Contact component will render here */}
+        <Route path="/about" element={<About />} /> {/* Portfolio component will render here */}
+        <Route path="/services" element={<Services />} /> 
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
