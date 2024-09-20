ee47dd51f423d4c839b589ffedb66c5405a0fb2b
PRANSHU SINGH THAKUR <pspranshusingh15@gmail.com>
2024-09-20 11:14:28 +0530

    Changes in css and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <a href="#home">Kala Kendra</a>
-      </div>
-      <ul className="navbar-links">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <nav className="navbar" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/image/logo/Untitled-1.png)` }}>
+
+    <div className="navbar-brand">
+    <Link to="/">
+    <img src={`${process.env.PUBLIC_URL}/image/logo/icon green.png`} alt="Kala Kendra Logo" className="navbar-logo" />
+
+      </Link>
+    </div>
+    <ul className="navbar-links">
+      <li>
+        <Link to="/">HOME</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">PORTFOLIO</Link>
+      </li>
+      <li>
+        <Link to="/about">ABOUT</Link>
+      </li>
+      <li>
+        <Link to="/services">SERVICES</Link>
+      </li>
+      <li>
+        <Link to="/review">REVIEWS</Link>
+      </li>
+      <li>
+        <Link to="/contact">CONTACT</Link>
+      </li>
+    </ul>
+  </nav>
   );
 };
 
